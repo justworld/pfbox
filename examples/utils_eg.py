f2bf32749f6e21cdfd3675ef83015529ccd3327d
justworld <jiangui1376044@126.com>
2018-10-15 09:32:01 +0800

    tornado import_object

--- examples/utils_eg.py
+++ examples/utils_eg.py
@@ -2,9 +2,3 @@
 """
 工具框架使用示例
 """
-
-#######  import example  ######
-from pythonframework.utils.import_util import import_object
-
-join = import_object('os.path.join')
-print join('fold', 'file')
