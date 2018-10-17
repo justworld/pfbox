0dc43c4661b75eb25494d2fed72e378a1561a598
justworld <jiangui1376044@126.com>
2018-10-17 09:41:31 +0800

    tornado configurable

--- pythonframework/utils/config_util.py
+++ pythonframework/utils/config_util.py
@@ -8,6 +8,27 @@ from .import_util import import_object
 class Configurable(object):
     """
     from tornado
+    工厂方法模式实现
+    示例 >>examples>>utils_eg
+
+    Base class for configurable interfaces.
+
+    A configurable interface is an (abstract) class whose constructor
+    acts as a factory function for one of its implementation subclasses.
+    The implementation subclass as well as optional keyword arguments to
+    its initializer can be set globally at runtime with `configure`.
+
+    By using the constructor as the factory method, the interface
+    looks like a normal class, `isinstance` works as usual, etc.  This
+    pattern is most useful when the choice of implementation is likely
+    to be a global decision (e.g. when `~select.epoll` is available,
+    always use it instead of `~select.select`), or when a
+    previously-monolithic class has been split into specialized
+    subclasses.
+
+    Configurable subclasses must define the class methods
+    `configurable_base` and `configurable_default`, and use the instance
+    method `initialize` instead of ``__init__``
     """
     __impl_class = None
     __impl_kwargs = None
@@ -25,23 +46,39 @@ class Configurable(object):
         if impl.configurable_base() is not base:
             return impl(*args, **init_kwargs)
         instance = super(Configurable, cls).__new__(impl)
+        # singleton magic.  If we get rid of that we can switch to __init__ here too.
         instance.initialize(*args, **init_kwargs)
         return instance
 
     @classmethod
     def configurable_base(cls):
+        """Returns the base class of a configurable hierarchy.
+
+        This will normally return the class in which it is defined.
+        (which is *not* necessarily the same as the cls classmethod parameter).
+        """
         raise NotImplementedError()
 
     @classmethod
     def configurable_default(cls):
+        """Returns the implementation class to be used if none is configured."""
         raise NotImplementedError()
 
     def initialize(self):
         """
+        Initialize a `Configurable` subclass instance.
+
+        Configurable classes should use `initialize` instead of ``__init__``.
         """
 
     @classmethod
     def configure(cls, impl, **kwargs):
+        """Sets the class to use when the base class is instantiated.
+
+        Keyword arguments will be saved and added to the arguments passed
+        to the constructor.  This can be used to set global defaults for
+        ome parameters.
+        """
         base = cls.configurable_base()
         if isinstance(impl, (str, unicode)):
             impl = import_object(impl)
@@ -52,6 +89,7 @@ class Configurable(object):
 
     @classmethod
     def configured_class(cls):
+        """Returns the currently configured class."""
         base = cls.configurable_base()
         if base.__dict__.get('_Configurable__impl_class') is None:
             base.__impl_class = cls.configurable_default()
